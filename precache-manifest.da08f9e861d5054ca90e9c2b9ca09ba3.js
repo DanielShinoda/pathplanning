43293beedb9fbe7f31b02c9592402ed242b0da1d
Daniel Khaybulin <danielkhaybulin@gmail.com>
2021-03-29 23:18:10 +0300

    Updates

---
+++ precache-manifest.da08f9e861d5054ca90e9c2b9ca09ba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "997c865ae47d1ea74adb3cd3682d29e8",
+    "url": "/pathplanning/index.html"
+  },
+  {
+    "revision": "49f40742e4d492d3bcb5",
+    "url": "/pathplanning/static/css/main.06521022.chunk.css"
+  },
+  {
+    "revision": "b1eeec30a0de0973778d",
+    "url": "/pathplanning/static/js/2.b0a291ff.chunk.js"
+  },
+  {
+    "revision": "49f40742e4d492d3bcb5",
+    "url": "/pathplanning/static/js/main.5698639c.chunk.js"
+  },
+  {
+    "revision": "d554e5c4e288876ecfa4",
+    "url": "/pathplanning/static/js/runtime-main.3fa035c8.js"
+  }
+]);
